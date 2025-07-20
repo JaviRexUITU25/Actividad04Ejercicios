14e820e572cea0b1d234e24f7c6aeaa40ea3202a
André <javiandre2007@gmail.com>
2025-07-20 13:46:12 -0600

    commit github

--- Ejerciciosact04.py
+++ Ejerciciosact04.py
@@ -1,2 +1,17 @@
 #Simulador de votacion cruzada
-
+print("---------BIENVENIDO AL REGISTRO DE VOTACIONES---------")
+name = input("Ingrese nombre completo: ")
+if len(name) < 5:
+    print("Nombre invalido")
+else:
+    dep = input("Ingrese su departamento: ")
+    if dep == "Peten" or dep == "Alta verapaz":
+        año_nacimiento = int(input("Ingrese su año de nacimiento: "))
+        if año_nacimiento > 2006 or dep != "Peten" or dep != "Alta verapaz":
+            print("Debe ser mayor de edad para votar.")
+        else:
+            ID = (input("Ingrese su ID: "))
+            if len(ID) != 13:
+                print("DPI invalido, intente de nuevo.")
+            else:
+                print(f"Bienvenido{name}, su centro de votacion es {dep}")
