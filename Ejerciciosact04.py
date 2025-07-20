951e27e98fdd6d2f4a1a034f1099b3ab0ba1afb5
André <javiandre2007@gmail.com>
2025-07-20 14:08:07 -0600

    Ejercicio 1

--- Ejerciciosact04.py
+++ Ejerciciosact04.py
@@ -5,13 +5,16 @@ if len(name) < 5:
     print("Nombre invalido")
 else:
     dep = input("Ingrese su departamento: ")
-    if dep == "Peten" or dep == "Alta verapaz":
-        año_nacimiento = int(input("Ingrese su año de nacimiento: "))
-        if año_nacimiento > 2006 or dep != "Peten" or dep != "Alta verapaz":
-            print("Debe ser mayor de edad para votar.")
+    año_nacimiento = int(input("Ingrese su año de nacimiento: "))
+    if año_nacimiento > 2006 and dep != "Peten" and dep != "Alta verapaz":
+        print("Debe ser mayor de edad para votar.")
+    else:
+        ID = (input("Ingrese su ID: "))
+        if len(ID) != 13:
+            print("DPI invalido, intente de nuevo.")
         else:
-            ID = (input("Ingrese su ID: "))
-            if len(ID) != 13:
-                print("DPI invalido, intente de nuevo.")
-            else:
-                print(f"Bienvenido{name}, su centro de votacion es {dep}")
+            print(f"Bienvenido {name}, su centro de votacion es {dep}")
+    if año_nacimiento > 2007:
+        print("Debe ser mayor de edad para votar.")
+
+
