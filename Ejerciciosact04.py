330cb2dc012b4a7f019da943f2ce0e71989a59ea
André <javiandre2007@gmail.com>
2025-07-20 21:14:58 -0600

    ejercicio 4

--- Ejerciciosact04.py
+++ Ejerciciosact04.py
@@ -55,9 +55,9 @@ while numero_intentos > 0:
         print("----------------------------ACCESO BLOQUEADO----------------------------")
         break'''
 
-#Simulador de facturacion con IVA, descuentos y propina
-milk = "30"
-eggs = "40"
+'''#Simulador de facturacion con IVA, descuentos y propina
+milk = 30
+eggs = 40
 shopping = []
 while True:
     print("-----MENU DE PRODUCTOS------\n"
@@ -74,6 +74,35 @@ while True:
             print("La leche ha sido agregada al carrito\n"
                   "1.Si\n"
                   "2.No\n")
+        total = int(input("1.Si\n"
+                          "2.No\n"
+                          "¿Desea dejar propina?: "))
+        if total == 1:
+            tip = int(input("Cuanto % de propina quiere agregar?: "))
+            frequent = int(input("1. Si\n"
+                                 "2. No\n"
+                                 "¿Tiene tarjeta de cliente frecuente?: "))
+            if frequent == 1:
+                    print (f"Su total con descuento es: Q.{(milk * 10)/100} \n")
+            elif frequent == 2:
+                print(f"Su total es de: Q.{milk}\n ")
+        elif total == 2:
+            frequent = int(input("1. Si\n"
+                                 "2. No\n"
+                                 "¿Tiene tarjeta de cliente frecuente?: "))
+            if frequent == 1:
+                    print (f"Su total con descuento es: Q.{(milk * 10)/100}\n")
+            if frequent == 2:
+                print(f"Su total es de: Q.{milk}\n ")
+    elif request == 2:
+        print(f"Los huevos tienen un valor de Q{eggs}.\n"
+              "1. Si\n"
+            "2. No\n")
+        price1= int(input("¿Desea añadirla?: "))
+        if price1 == 1:
+            print("Los huevos han sido agregados al carrito\n"
+                  "1.Si\n"
+                  "2.No\n")
         final_price = int(input("¿Desea seguir comprando?: "))
         if final_price == 1:
              print("")
@@ -81,30 +110,36 @@ while True:
         if price1 == 2:
             print("1. Si\n"
                 "2.No\n")
-            total = int(input("¿Desea dejar propina?: "))
-            if total == 1:
-                tip = int(input("Cuando % de propina quiere agregar?: "))
-            elif total == 2:
-                frequent = int(input("¿Tiene tarjeta de cliente frecuente?\n"
-                                         "1. Si\n"
-                                         "2. No"))
-                if frequent == "1":
-                    discount = (f"Su total con descuento es: {(final_price * 10)
-                                                                  /100}\n")
-                elif request == "2":
-                    total = (f"Su total es de: {final_price}\n ")
-                    iva_add = final_price + (final_price * 12) / 100
-    elif request == "2":
-        print(f"Los huevos tienen un valor de Q{eggs}.\n"
-              "1. Si\n"
-            "2. No\n")
-        price1= int(input("¿Desea añadirla?: "))
-        iva_add = final_price + (final_price * 12) / 100
+        total = int(input("1.Si\n"
+                          "2.No\n"
+                          "¿Desea dejar propina?: "))
+        if total == 1:
+            tip = int(input("Cuanto % de propina quiere agregar?: "))
+            frequent = int(input("1. Si\n"
+                                 "2. No\n"
+                                 "¿Tiene tarjeta de cliente frecuente?: "))
+            if frequent == 1:
+                    print (f"Su total con descuento es: Q.{(eggs * 10)/100} \n")
+            elif frequent == 2:
+                print(f"Su total es de: {eggs}\n ")
+                iva_add = final_price + (final_price * 12) / 100
+        elif total == 2:
+            frequent = int(input("1. Si\n"
+                                 "2. No\n"
+                                 "¿Tiene tarjeta de cliente frecuente?: "))
+            if frequent == 1:
+                    print (f"Su total con descuento es: Q.{(eggs * 10)/100}\n")
+            if frequent == 2:
+                print(f"Su total es de: Q.{eggs}\n ")
+                iva_add = final_price + (eggs * 12) / 100
     else:
-            factura =int(input(""))
-'''
-#Verificador de fecha válida
+            factura =int(input(""))'''
+
+
+'''#Verificador de fecha válida
 date = input("Ingrese el numero del mes y del dia: ", end= '')
 mes, dia = map(int, input().split())
-semana = {1: "Lunes", 2: "Martes", 3: "Miercoles", 4: "Jueves", 5: "Viernes", 6: "Sabado", 7: "Domingo"}'''
+semana = {1: "Lunes", 2: "Martes", 3: "Miercoles", 4: "Jueves", 5: "Viernes", 6: "Sabado", 7: "Domingo"}
 
+leche = 30
+print(f"El total de su compra es de: {leche}")'''
